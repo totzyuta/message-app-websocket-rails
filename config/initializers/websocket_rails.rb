e34b7ba46fd1c63332955b74b5b9c62d6f016f94
totzYuta <yuta.totz@gmail.com>
2015-12-03 09:14:19 +0900

    Configure stand alone server mode

--- config/initializers/websocket_rails.rb
+++ config/initializers/websocket_rails.rb
@@ -15,7 +15,8 @@ WebsocketRails.setup do |config|
   # Change to true to enable standalone server mode
   # Start the standalone server with rake websocket_rails:start_server
   # * Requires Redis
-  config.standalone = false
+  config.standalone = true
+  config.standalone_port = 3001
 
   # Change to true to enable channel synchronization between
   # multiple server instances.
@@ -28,7 +29,7 @@ WebsocketRails.setup do |config|
   # Uncomment and edit to point to a different redis instance.
   # Will not be used unless standalone or synchronization mode
   # is enabled.
-  # config.redis_options = {:host => 'localhost', :port => '6379'}
+  config.redis_options = {:host => 'localhost', :port => '6379'}
 
   # By default, all subscribers in to a channel will be removed
   # when that channel is made private. If you don't wish active
@@ -37,7 +38,7 @@ WebsocketRails.setup do |config|
   # config.keep_subscribers_when_private = false
 
   # Set to true if you wish to broadcast channel subscriber_join and
-  # subscriber_part events. All subscribers of a channel will be 
+  # subscriber_part events. All subscribers of a channel will be
   # notified when other clients join and part the channel. If you are
   # using the UserManager, the current_user object will be sent along
   # with the event.
